5acdff6a41667a3a997fafa3b5d875198944a8cd
Angelos Katharopoulos <katharas@gmail.com>
2016-02-29 22:31:47 +0200

    Add Events
    TODO: Test them

---
+++ include/Events.hpp
@@ -0,0 +1,98 @@
+#ifndef _EVENTS_HPP_
+#define _EVENTS_HPP_
+
+
+#include <memory>
+#include <list>
+
+
+/**
+ * A base event object that will be dispatched and received.
+ */
+class Event
+{
+    public:
+        Event(std::string id);
+
+        const std::string & id() const;
+
+    private:
+        std::string id_;
+};
+
+
+/**
+ * A simple event listener interface.
+ */
+class IEventListener
+{
+    public:
+        virtual void on_event(std::shared_ptr<Event> event) = 0;
+};
+
+
+/**
+ * Creates an event listener from a function with the same signature.
+ */
+class FunctionEventListener : public IEventListener
+{
+    public:
+        FunctionEventListener(std::function<void(std::shared_ptr<Event>)> listener);
+
+        void on_event(std::shared_ptr<Event> event) override;
+
+    private:
+        std::function<void(std::shared_ptr<Event>)> listener_;
+};
+
+
+/**
+ * IEventDispatcher is the interface of a very simple event dispatcher.
+ */
+class IEventDispatcher
+{
+    public:
+        
+        virtual void add_listener(std::shared_ptr<IEventListener> listener) = 0;
+        virtual void remove_listener(std::shared_ptr<IEventListener> listener) = 0;
+        virtual void dispatch(std::shared_ptr<Event> event) = 0;
+
+        /**
+         * The created object is returned so that the listener can be removed.
+         */
+        std::shared_ptr<IEventListener> add_listener(
+            std::function<void(std::shared_ptr<Event>)> listener
+        ) {
+            auto l = std::make_shared<FunctionEventListener>(listener);
+
+            add_listener(l);
+
+            return l;
+        }
+
+        template <class EventType, typename... Args>
+        void dispatch(Args... args) {
+            auto event = std::make_shared<EventType>(args...);
+
+            dispatch(event);
+        }
+};
+
+
+/**
+ * EventDispatcher is a simple implementation of an IEventDispatcher. It can be
+ * copied, passed by value, reference whatever. It is not thread safe.
+ */
+class EventDispatcher : public IEventDispatcher
+{
+    public:
+        void add_listener(std::shared_ptr<IEventListener> listener);
+        void remove_listener(std::shared_ptr<IEventListener> listener);
+        void dispatch(std::shared_ptr<Event> event);
+
+    private:
+        std::list<std::shared_ptr<IEventListener> > listeners_;
+};
+
+
+#endif  // _EVENTS_HPP_
---
+++ src/Events.cpp
@@ -0,0 +1,35 @@
+
+#include "Events.hpp"
+
+
+FunctionEventListener::FunctionEventListener(
+    std::function<void(std::shared_ptr<Event>)> listener
+) : listener_(listener)
+{}
+
+
+void FunctionEventListener::on_event(std::shared_ptr<Event> event) {
+    listener_(event);
+}
+
+
+void EventDispatcher::add_listener(std::shared_ptr<IEventListener> listener) {
+    listeners_.push_back(listener);
+}
+
+
+void EventDispatcher::remove_listener(std::shared_ptr<IEventListener> listener) {
+    for (auto it = listeners_.begin(); it != listeners_.end(); it++) {
+        if (*it == listener) {
+            listeners_.erase(it);
+            break;
+        }
+    }
+}
+
+
+void EventDispatcher::dispatch(std::shared_ptr<Event> event) {
+    for (auto l : listeners_) {
+        l->on_event(event);
+    }
+}
