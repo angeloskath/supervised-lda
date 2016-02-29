05621a6dc8060491f605c1d58e7ea5c2e4a724b6
Paschalidou Despoina <paschalidoud@gmail.com>
2016-02-29 17:27:37 +0200

    Add implementation for SeededInitialization

--- include/SeededInitialization.hpp
+++ include/SeededInitialization.hpp
@@ -1,28 +1,20 @@
-#ifndef _IINITIALIZATION_HPP
-#define _IINITIALIZATION_HPP
+#ifndef _SEEDEDINITIALIZATION_HPP_
+#define _SEEDEDINITIALIZATION_HPP_
 
-#include <Eigen/Core>
-
-#include "ISerializable.hpp"
-
-using namespace Eigen;
+#include "IInitialization.hpp"
 
 template <typename Scalar>
-class IInitialization : public ISerializable<Scalar>
+class SeededInitialization : public IInitialization<Scalar>
 {
     typedef Matrix<Scalar, Dynamic, Dynamic> MatrixX;
     typedef Matrix<Scalar, Dynamic, 1> VectorX;
     
     public:
-        enum Type
-        {
-            Seeded = 0,
-            Random
-        };
-
+        SeededInitialization(size_t topics) : topics_(topics) {};
+        
         /**
           * Function used for the initialization of model parameters, namely
-          * alpha, beta, eta
+          * alpha, beta, eta in a seeded way.
           *
           * @param X The word counts in column-major order for a single document
           * @param y The class label as integer for the current document
@@ -30,13 +22,16 @@ class IInitialization : public ISerializable<Scalar>
           * @param beta The over word topic distributiosn
           * @param eta The classification parameters
           */
-        virtual void initialize_model_parameters(
+        void initialize_model_parameters(
             const MatrixXi &X,
             const VectorXi &y,
             Ref<VectorX> alpha,
             Ref<MatrixX> beta,
             Ref<MatrixX> eta
-        )=0;
-};
+        );
 
-#endif  // _IINITIALIZATION_HPP
+    private:
+        // The total number of topics used
+        size_t topics_; 
+};
+#endif  // _SEEDEDINITIALIZATION_HPP_
---
+++ src/SeededInitialization.cpp
@@ -0,0 +1,34 @@
+#include <random>
+
+#include "SeededInitialization.hpp"
+
+template <typename Scalar>
+void SeededInitialization<Scalar>::initialize_model_parameters(
+    const MatrixXi &X,
+    const VectorXi &y,
+    Ref<VectorX> alpha,
+    Ref<MatrixX> beta,
+    Ref<MatrixX> eta
+) {
+    alpha = VectorX::Constant(topics_, 1.0 / topics_);
+    // Eigen has no unique function, therefore we use maxcoeff instead to
+    // calculate the number of classes, which is C
+    eta = MatrixX::Zero(topics_, y.maxCoeff() + 1);
+    
+    beta = MatrixX::Constant(topics_, X.rows(), 1);
+    std::mt19937 rng;
+    rng.seed(0);
+    std::uniform_int_distribution<> initializations(10, 50);
+    std::uniform_int_distribution<> document(0, X.cols()-1);
+    auto N = initializations(rng);
+
+    // Initialize _beta
+    for (int k=0; k<topics_; k++) {
+        // Choose randomly a bunch of documents to initialize beta
+        for (int r=0; r<N; r++) {
+            beta.row(k) += X.cast<Scalar>().col(document(rng)).transpose();
+        }
+        beta.row(k) = beta.row(k) / beta.row(k).sum();
+    }
+}
+
