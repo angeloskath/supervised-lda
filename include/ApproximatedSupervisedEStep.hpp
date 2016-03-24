407997535c7c891fd90d5146bc20186ff4ef9327
Paschalidou Despoina <paschalidoud@gmail.com>
2016-03-24 21:34:06 +0200

    Add ApproximatedSupervisedEstep

---
+++ include/ApproximatedSupervisedEStep.hpp
@@ -0,0 +1,52 @@
+#ifndef _APPROXIMATEDSUPERVISEDESTEP_HPP_
+#define _APPROXIMATEDSUPERVISEDESTEP_HPP_
+
+#include "UnsupervisedEStep.hpp"
+
+template<typename Scalar>
+class ApproximatedSupervisedEStep : public UnsupervisedEStep<Scalar>
+{
+    typedef Matrix<Scalar, Dynamic, Dynamic> MatrixX;
+    typedef Matrix<Scalar, Dynamic, 1> VectorX;
+
+    public:
+        ApproximatedSupervisedEStep(
+            size_t e_step_iterations = 10,
+            Scalar e_step_tolerance = 1e-2,
+            size_t fixed_point_iterations = 20
+        );
+
+        /** Maximize the ELBO w.r.t phi and gamma
+         *
+         * We use the following update functions until convergence.
+         *
+         * \phi_n \prop \beta_{w_n} exp(
+         *      \Psi(\gamma) +
+         *      \frac{1}{N} \eta_y^T +
+         *      \frac{h}{h^T \phi_n^{old}}
+         *  )
+         * \gamma = \alpha + \sum_{n=1}^N \phi_n
+         *
+         * @param doc          A sinle document
+         * @param parameters   An instance of class Parameters, which
+         *                     contains all necessary model parameters 
+         *                     for e-step's implementation
+         * @return             The variational parameters for the current
+         *                     model, after e-step is completed
+         */
+        std::shared_ptr<Parameters> doc_e_step(
+            const std::shared_ptr<Document> doc,
+            const std::shared_ptr<Parameters> parameters
+        ) override;
+
+    private:
+        // The maximum number of iterations in E-step
+        size_t e_step_iterations_;
+        // The maximum number of iterations while maximizing phi in E-step
+        size_t fixed_point_iterations_;
+        // The convergence tolerance for the maximazation of the ELBO w.r.t.
+        // phi and gamma in E-step
+        Scalar e_step_tolerance_;
+    
+};
+#endif   // _APPROXIMATEDSUPERVISEDESTEP_HPP_
---
+++ src/ApproximatedSupervisedEStep.cpp
@@ -0,0 +1,83 @@
+#include "ProgressEvents.hpp"
+#include "ApproximatedSupervisedEStep.hpp"
+#include "e_step_utils.hpp"
+#include "utils.hpp"
+
+template <typename Scalar>
+ApproximatedSupervisedEStep<Scalar>::ApproximatedSupervisedEStep(
+    size_t e_step_iterations,
+    Scalar e_step_tolerance,
+    size_t fixed_point_iterations
+) {
+    e_step_iterations_ = e_step_iterations;
+    fixed_point_iterations_ = fixed_point_iterations;
+    e_step_tolerance_ = e_step_tolerance;
+}
+
+template <typename Scalar>
+std::shared_ptr<Parameters> ApproximatedSupervisedEStep<Scalar>::doc_e_step(
+    const std::shared_ptr<Document> doc,
+    const std::shared_ptr<Parameters> parameters
+) {
+    // Words form Document doc
+    const VectorXi &X = doc->get_words();
+    int num_words = X.sum();
+    int voc_size = X.rows();
+    VectorX X_ratio = X.cast<Scalar>() / num_words;
+
+    // Get the document's class
+    int y = std::static_pointer_cast<ClassificationDocument>(doc)->get_class();
+
+    // Cast parameters to model parameters in order to save all necessary
+    // matrixes
+    const VectorX &alpha = std::static_pointer_cast<SupervisedModelParameters<Scalar> >(parameters)->alpha;
+    const MatrixX &beta = std::static_pointer_cast<SupervisedModelParameters<Scalar> >(parameters)->beta;
+    const MatrixX &eta = std::static_pointer_cast<SupervisedModelParameters<Scalar> >(parameters)->eta;
+    int num_topics = beta.rows();
+
+    // The variational parameters to be computed
+    MatrixX phi = MatrixX::Constant(num_topics, voc_size, 1.0/num_topics);
+    VectorX gamma = alpha.array() + static_cast<Scalar>(num_words)/num_topics;
+
+    // to check for convergence
+    Scalar old_likelihood = -INFINITY, new_likelihood = -INFINITY;
+
+    for (size_t iteration=0; iteration<e_step_iterations_; iteration++) {
+
+        new_likelihood = e_step_utils::compute_supervised_likelihood<Scalar>(
+            X,
+            y,
+            alpha,
+            beta,
+            eta,
+            phi,
+            gamma
+        );
+        if ((new_likelihood - old_likelihood)/(-old_likelihood) < e_step_tolerance_) {
+            break;
+        }
+        old_likelihood = new_likelihood;
+
+        e_step_utils::compute_supervised_approximate_phi<Scalar>(
+            X_ratio,
+            y,
+            beta,
+            eta,
+            gamma,
+            phi
+        );
+
+        // Equation (6) in Supervised topic models, Blei, McAulife 2008
+        e_step_utils::compute_gamma<Scalar>(X, alpha, phi, gamma);
+    }
+
+    // notify that the e step has finished
+    this->get_event_dispatcher()->template dispatch<ExpectationProgressEvent<Scalar> >(new_likelihood);
+
+    return std::make_shared<VariationalParameters<Scalar> >(gamma, phi);
+}
+
+// Template instantiation
+template class ApproximatedSupervisedEStep<float>;
+template class ApproximatedSupervisedEStep<double>;
+
